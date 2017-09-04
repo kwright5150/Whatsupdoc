bd4326b034ea73a2433e3bdffbaa71e9df02cdcc
kwright5150 <kvvincentvalentine@gmail.com>
2017-09-04 13:43:35 -0600

    Suricata script

--- suri.sh
+++ suri.sh
@@ -5,7 +5,7 @@ echo Refreshing repositories...
 sudo apt-get update 
  
 echo Installing Suricata dependencies... 
-sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config wget 
+sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config wget net-tools tcpdump 
  
 echo Downloading sourcecode.. 
 sudo wget https://www.openinfosecfoundation.org/download/suricata-4.0.0.tar.gz 
